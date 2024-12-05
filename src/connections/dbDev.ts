7cd5e22be51b59a481c3a570b99a3ce55506ed2d
LucasSanSo <namikazefabi@gmail.com>
2024-12-05 20:00:43 -0300

    qqq

--- src/connections/dbDev.ts
+++ src/connections/dbDev.ts
@@ -1,28 +1,15 @@
 import { DataSource } from "typeorm";
 import { Task } from "../models/task";
 
-// Local Environment - Ambiente local
-// export const DevDataSource = new DataSource({
-//     type: "postgres",
-//     host: "localhost",
-//     port: 5432,
-//     username: "postgres",
-//     password: "postgres",
-//     database: "backend",
-//     entities: [Task]
-// })
-
-// // Production Environment - AMBIENTE PRODUÇÃO
+//Local Environment - Ambiente local
 export const DevDataSource = new DataSource({
     type: "postgres",
-    host: "dpg-csg29v5umphs73ag6m80-a.oregon-postgres.render.com",
+    host: "localhost",
     port: 5432,
-    username: "tasks_db_xeip_user",
-    password: "711MdxmDlhaxUE6DTpZhJiDDY0EuViQL",
-    database: "tasks_db_xeip",
-    extra: {
-        options: "-c statement_timeout=30000ms -c search_path=monsters",
-      },
-    // url: "postgresql://tasks_db_xeip_user:711MdxmDlhaxUE6DTpZhJiDDY0EuViQL@dpg-csg29v5umphs73ag6m80-a/tasks_db_xeip",
+    username: "postgres",
+    password: "postgres",
+    database: "backend",
     entities: [Task]
 })
+
+// // Production Environment - AMBIENTE PRODUÇÃO
